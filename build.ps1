b512c569638bb37f17f3e13387de9a9a6cd84c33
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-05-01 23:22:56 +0200

    auto update version number

--- build.ps1
+++ build.ps1
@@ -10,10 +10,21 @@ docker buildx version | Out-Null
 # Set up Docker Buildx builder
 docker buildx create --use --name tucan-builder --driver docker-container --bootstrap 2>$null | Out-Null
 
-# Fetch the latest version from Git tags
+# Fetch the latest version or initialize if not found
 $latestVersion = git describe --tags --abbrev=0 2>$null
 if (-Not $latestVersion) { $latestVersion = "0.1.0" }
 
+# Increment the version number
+$versionParts = $latestVersion -split '\.'
+$versionParts[2] = [int]$versionParts[2] + 1
+$newVersion = "$($versionParts[0]).$($versionParts[1]).$($versionParts[2])"
+Write-Host "New version: $newVersion"
+
 # Build and push the Docker image
-docker buildx build --platform $platforms -t "${imageName}:${latestVersion}" -t "${imageName}:latest" --push .
+docker buildx build --platform $platforms -t "${imageName}:${newVersion}" -t "${imageName}:latest" --push .
 Write-Host "Build and push completed successfully."
+
+# Push new Tags
+git tag $newVersion
+git push origin $newVersion
+Write-Host "Pushed new tag: $newVersion"
